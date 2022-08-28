141c6cba73987c355cdaeeea555dccfda95770c1
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-08-28 22:45:38 +0400

    textcontent and classlist added for start app

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceImg = document.querySelector('.dice');
+
+//Initialise the app
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceImg.classList.add('hidden');
+
+
